863f38547d82b4c7812f6187f3aee1d6d4ac944e
R33my <reemysteam@gmail.com>
2022-05-17 15:36:11 +0300

    Change

--- Two-dimensional Arrays/Source.cpp
+++ Two-dimensional Arrays/Source.cpp
@@ -1,9 +1,36 @@
 #include<iostream>
 
+//Самостійне завдання 84A. Варіант 13.
 using namespace std;
 int main()
 {
+	srand(time(NULL));
 
+	const int n = 4;
+	int array[n][n];
 
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (j + i == n - 1)
+			{
+				array[i][j] = n - j;
+			}	
+			else
+			{
+				array[i][j] = 0;
+			}	
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << " " << array[i][j];
+		}
+		cout << endl;
+	}
 
 }
